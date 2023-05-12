b4521989890aba4ad918162a1bb444d586c7dbf5
Musyimi <musyimi13@gmail.com>
2023-05-12 14:27:07 +0300

    First commit

---
+++ lib/repair.ex
@@ -0,0 +1,18 @@
+defmodule Repair do
+  @moduledoc """
+  Documentation for `Repair`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Repair.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/repair_test.exs
@@ -0,0 +1,8 @@
+defmodule RepairTest do
+  use ExUnit.Case
+  doctest Repair
+
+  test "greets the world" do
+    assert Repair.hello() == :world
+  end
+end
